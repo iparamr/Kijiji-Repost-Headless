f8757a256142bd818be3d79ab813839b5bd688d5
Param Randhawa <param.randhawa@hotmail.com>
2020-07-05 16:35:48 -0400

    GIT-000 Add new listings for Vacuum, S4 Parts, Cables and Adaptors, Silk Tie

--- ad-poster.sh
+++ ad-poster.sh
@@ -2,41 +2,53 @@ echo "Enter your Kijiji username (email):"
 read USER
 echo "Enter your Kijiji password:"
 read PASS
-
+echo
 echo "==== SHOWING LIVE ADS! ===="
 python3 kijiji_repost_headless -u $USER -p $PASS show --key id
-
+echo
 echo "==== DELETE ALL ADS! ===="
 python3 kijiji_repost_headless -u $USER -p $PASS nuke
-
+echo
 echo "==== REPOST ALL ADS! ===="
-
+echo
 echo "==== BOOKS ===="
 python3 kijiji_repost_headless -u $USER -p $PASS repost _ads/books.yaml
-
+echo
 echo "==== CAMERA ===="
 python3 kijiji_repost_headless -u $USER -p $PASS post _ads/camera.yaml
-
+echo
 echo "==== MACBOOK PRO ===="
 python3 kijiji_repost_headless -u $USER -p $PASS post _ads/macbook_pro.yaml
-
+echo
 echo "==== FITBIT ===="
 python3 kijiji_repost_headless -u $USER -p $PASS post _ads/fitbit.yaml
-
+echo
 echo "==== IPHONE 7 PLUS ===="
 python3 kijiji_repost_headless -u $USER -p $PASS post _ads/iphone_7_plus.yaml
-
+echo
 echo "==== IPHONE CASE (SPIGEN) 8 PLUS / XS MAX ===="
 python3 kijiji_repost_headless -u $USER -p $PASS post _ads/iphone_case_8_xs_max.yaml
-
+echo
 echo "==== SAMSUNG GALAXY S6 ===="
 python3 kijiji_repost_headless -u $USER -p $PASS post _ads/samsung_s6.yaml
-
+echo
 echo "==== SAMSUNG GALAXY S6 FLIP CASE ===="
-python3 kijiji_repost_headless -u $USER -p $PASS post _ads/samsung_case_s6_flip_cover.yaml
-
+python3 kijiji_repost_headless -u $USER -p $PASS post _ads/samsung_case_s6_flip_case.yaml
+echo
 echo "==== SAMSUNG GALAXY S6 CLEAR CASE ===="
 python3 kijiji_repost_headless -u $USER -p $PASS post _ads/samsung_case_s6_clear_case.yaml
-
+echo
+echo "==== SAMSUNG GALAXY S4 ===="
+python3 kijiji_repost_headless -u $USER -p $PASS post _ads/samsung_s4_parts.yaml
+echo
 echo "==== LOGITECH PERFORMANCE MX MOUSE ===="
 python3 kijiji_repost_headless -u $USER -p $PASS post _ads/logitech_mx_mouse.yaml
+echo
+echo "==== CABLES AND ADAPTORS ===="
+python3 kijiji_repost_headless -u $USER -p $PASS post _ads/cables_and_adaptors.yaml
+echo
+echo "==== EUREKA VACUUM CLEANER ===="
+python3 kijiji_repost_headless -u $USER -p $PASS post _ads/vacuum_cleaner.yaml
+echo
+echo "==== SILK TIE ===="
+python3 kijiji_repost_headless -u $USER -p $PASS post _ads/silk_tie.yaml
