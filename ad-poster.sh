fdd7d75725fd95ec849362280fd18702ddae59d8
Param Randhawa <param.randhawa@hotmail.com>
2020-07-05 14:45:35 -0400

    GIT-000 Add new listings for iPhone 7 Plus, Logitech Mouse, Samsung S6

--- ad-poster.sh
+++ ad-poster.sh
@@ -19,3 +19,12 @@ python3 kijiji_repost_headless -u $USER -p $PASS repost _ads/macbook_pro.yaml
 
 echo "==== FITBIT ===="
 python3 kijiji_repost_headless -u $USER -p $PASS repost _ads/fitbit.yaml
+
+echo "==== IPHONE 7 PLUS ===="
+python3 kijiji_repost_headless -u $USER -p $PASS repost _ads/iphone_7_plus.yaml
+
+echo "==== SAMSUNG GALAXY S6 ===="
+python3 kijiji_repost_headless -u $USER -p $PASS repost _ads/samsung_s6.yaml
+
+echo "==== LOGITECH PERFORMANCE MX MOUSE ===="
+python3 kijiji_repost_headless -u $USER -p $PASS repost _ads/logitech_mx_mouse.yaml
