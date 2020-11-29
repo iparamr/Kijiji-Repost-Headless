d53e5df6a2e8f98a4c2975e3be318a6a528e5a29
Param Randhawa <param.randhawa@hotmail.com>
2020-11-29 17:16:03 -0500

    Reprioritize ads

--- ad-poster.sh
+++ ad-poster.sh
@@ -1,4 +1,4 @@
-SLEEP=3
+SLEEP=1
 echo "Showing all live ads..."
 python3 kijiji_repost_headless -s ssid.txt show --key id
 echo "Deleting all ads..."
@@ -6,8 +6,8 @@ python3 kijiji_repost_headless -s ssid.txt nuke
 echo "Reposting all ads..."
 echo
 sleep $SLEEP
-echo "BOOKS"
-python3 kijiji_repost_headless -s ssid.txt post _ads/books.yaml
+echo "IKEA MATTRESS"
+python3 kijiji_repost_headless -s ssid.txt post _ads/mattress.yaml
 echo
 sleep $SLEEP
 echo "CANON CAMERA"
@@ -25,12 +25,24 @@ echo "IPHONE 7 PLUS"
 python3 kijiji_repost_headless -s ssid.txt post _ads/iphone_7_plus.yaml
 echo
 sleep $SLEEP
-echo "IPHONE CASE (SPIGEN) 8 PLUS / XS MAX"
-python3 kijiji_repost_headless -s ssid.txt post _ads/iphone_case_8_xs_max.yaml
-echo
-sleep $SLEEP
 echo "SAMSUNG GALAXY S6"
 python3 kijiji_repost_headless -s ssid.txt post _ads/samsung_s6.yaml
+echo
+sleep $SLEEP
+echo "LOGITECH PERFORMANCE MX MOUSE"
+python3 kijiji_repost_headless -s ssid.txt post _ads/logitech_mx_mouse.yaml
+echo
+sleep $SLEEP
+echo "EUREKA VACUUM CLEANER"
+python3 kijiji_repost_headless -s ssid.txt post _ads/vacuum_cleaner.yaml
+echo
+sleep $SLEEP
+echo "BOOKS"
+python3 kijiji_repost_headless -s ssid.txt post _ads/books.yaml
+echo
+sleep $SLEEP
+echo "IPHONE CASE (SPIGEN) 8 PLUS / XS MAX"
+python3 kijiji_repost_headless -s ssid.txt post _ads/iphone_case_8_xs_max.yaml
 echo
 sleep $SLEEP
 echo "SAMSUNG GALAXY S6 FLIP CASE"
@@ -45,21 +57,9 @@ echo "SAMSUNG GALAXY S4"
 python3 kijiji_repost_headless -s ssid.txt post _ads/samsung_s4_parts.yaml
 echo
 sleep $SLEEP
-echo "LOGITECH PERFORMANCE MX MOUSE"
-python3 kijiji_repost_headless -s ssid.txt post _ads/logitech_mx_mouse.yaml
-echo
-sleep $SLEEP
 echo "CABLES AND ADAPTORS"
 python3 kijiji_repost_headless -s ssid.txt post _ads/cables_and_adaptors.yaml
 echo
 sleep $SLEEP
-echo "EUREKA VACUUM CLEANER"
-python3 kijiji_repost_headless -s ssid.txt post _ads/vacuum_cleaner.yaml
-echo
-sleep $SLEEP
 echo "SILK TIE"
 python3 kijiji_repost_headless -s ssid.txt post _ads/silk_tie.yaml
-echo
-sleep $SLEEP
-echo "IKEA MATTRESS"
-python3 kijiji_repost_headless -s ssid.txt post _ads/mattress.yaml
